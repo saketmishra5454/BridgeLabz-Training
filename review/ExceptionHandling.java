121f7902d5224e9609406911fa5cc403fde5185d
Saket Mishra <saketmishra5454@gmail.com>
2026-01-22 17:21:28 +0530

    [Saket Mishra] Add .Added Excecption with multiple catch blocks in review

---
+++ review/ExceptionHandling.java
@@ -0,0 +1,46 @@
+
+// Custom Exception Class
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String msg) {
+        super(msg);
+    }
+}
+
+public class ExceptionHandling {
+
+    // Method throwing custom exception
+    static void checkAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above!");
+        } else {
+            System.out.println("Eligible to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Program Started...");
+
+        try {
+            int age = 15; // Custom exception trigger
+            int result = 10 / 0; // Arithmetic exception trigger
+            checkAge(age);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Caught ArithmeticException!");
+            System.out.println("Message: " + e.getMessage());
+
+        } catch (InvalidAgeException e) {
+            System.out.println("Caught Custom InvalidAgeException!");
+            System.out.println("Message: " + e.getMessage());
+
+        } catch (Exception e) {
+            System.out.println("Caught General Exception!");
+            System.out.println("Message: " + e.getMessage());
+        }
+
+        System.out.println("Program Ended Normally.");
+    }
+}
+
+
