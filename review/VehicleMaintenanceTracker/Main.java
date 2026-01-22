3662c877450f7c823cdf22ca31aae5141e8e169a
Saket Mishra <saketmishra5454@gmail.com>
2026-01-22 13:14:59 +0530

    [Saket Mishra]Add . Added some test cases for checking the performance in main

--- review/VehicleMaintenanceTracker/Main.java
+++ review/VehicleMaintenanceTracker/Main.java
@@ -1,6 +1,19 @@
 public class Main{
     public static void main(String[] args){
         Vehicle car1 = new Car("Sedan", 5000);         //this is a testcase given to us;
-        System.out.printf("%.2f%n", car1.getMilageCost());   
+        System.out.printf("%.2f%n", car1.getMilageCost());  
+
+        Vehicle truck1 = new Truck("Ashoka", 10000);         // testcase 1
+        System.out.printf("%.2f%n", truck1.getMilageCost());
+
+        Vehicle car2 = new Car("Tata Safari", 0);         // testcase 2;
+        System.out.printf("%.2f%n", car2.getMilageCost());
+
+
+
+
+
+
+
     }
 }
