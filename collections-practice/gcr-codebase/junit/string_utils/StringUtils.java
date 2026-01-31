9e49c6cc105654ed4a8a8ad949c098ee91f10e6b
Saket Mishra <saketmishra5454@gmail.com>
2026-01-31 22:47:01 +0530

    [Saket Mishra] Add. Added Solved regex problems

--- collections-practice/gcr-codebase/junit/string_utils/StringUtils.java
+++ collections-practice/gcr-codebase/junit/string_utils/StringUtils.java
@@ -1,4 +1,21 @@
 package junit.string_utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringUtils {
+    public static void main(String[] args) {
+        String regex = "^[A-Za-z][A-Za-z0-9_]{4,14}";
+        Pattern pattern = Pattern.compile(regex);
+        String[] userNames = {"user_123","123user","us"};
+        for(String name : userNames){
+            Matcher matcher = pattern.matcher(name);
+            if(matcher.matches()){
+                System.out.println(name+" -> Valid");
+            }
+            else{
+                System.out.println(name+" -> Invalid");
+            }
+        }
+    }
 }
