782b895df7ce78d685f21adc74e09f0385504dbb
Saket Mishra <saketmishra5454@gmail.com>
2026-02-04 01:27:12 +0530

    [Saket Mishra] Add .Added Junit solved problems in gcr-codebase

---
+++ collections-practice/gcr-codebase/junit/taskmanager/TaskManager.java
@@ -0,0 +1,11 @@
+package taskmanager;
+public class TaskManager {
+
+    // Long task
+    public String longRunningTask() throws Exception {
+
+        Thread.sleep(3000); // 3 seconds
+
+        return "Done";
+    }
+}
