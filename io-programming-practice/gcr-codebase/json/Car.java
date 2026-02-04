e97ddc14f57f5c605030c7f03fd6bc67df6c7421
Saket Mishra <saketmishra5454@gmail.com>
2026-02-04 23:36:32 +0530

    [Saket Mishra] Add .Added the JSON solved problems in gcr-codebase

---
+++ io-programming-practice/gcr-codebase/json/Car.java
@@ -0,0 +1,14 @@
+// Car class
+class Car {
+
+    String brand;
+    String model;
+    int year;
+
+    // Constructor
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+}
